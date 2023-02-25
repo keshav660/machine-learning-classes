a5887cff17878cac22783b06468dc1bfff2e360f
keshav660 <108389536+keshav660@users.noreply.github.com>
2023-02-25 19:03:44 +0530

    Add files via upload

---
+++ variable/Variable.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### VARIABLES
+# 
+# ###   Rules for naming the variable
+# ##### 1. variable can have alphabets[a-z,A-Z]
+# ##### 2. varaiable name should not start eith a number 
+# ##### 3. variabel do not allow special character other then underscore
+# ##### 4. case sensitive
+# 
+
+# In[3]:
+
+
+x = y = z = "test"
+z
+
+
+# In[4]:
+
+
+y
+
+
+# In[5]:
+
+
+Books = ["ds with python","ba with python"]
+Books
+
+
+# In[6]:
+
+
+books ## python is a case sensitive
+
+
+# In[7]:
+
+
+B2ooks = ["ds with python","ba with python"]
+B2ooks
+
+
+# In[8]:
+
+
+book*list = ["ds with python","ba with python"]
+book*list                                          ## can not use special character
+
+
+# ### Types of Analytics
+# ##### 1. Descriptive -> (what happened in the past)
+# ##### 2. Dignostic-> (why)
+# ##### 3 . predictive -> {future}
+# ##### 4. prescriptive-> {solution}
+
+# In[ ]:
+
+
+
+
